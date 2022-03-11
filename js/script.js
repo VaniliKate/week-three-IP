cdbe0e7086ac6c34d76d2b39c7a024ddca8328da
Vanili <katevanili98@gmail.com>
2022-03-11 20:43:35 +0300

    Modified the forms to receive input and give an alert

--- js/script.js
+++ js/script.js
@@ -53,13 +53,67 @@ function normal(mydiv) {
 }
 */
 
-//Form input
-const form = document.getElementById('form');
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const box = document.getElementById('test');
 
-function myPopUp() {
-    alert("We have received your message.Thank you for reaching out to us");
+$(document).ready(function () {
 
-}
+    // Validate name
+    $('#usercheck').hide();
+    let usernameError = true;
+    $('#usernames').keyup(function () {
+        validateUsername();
+    });
+
+    function validateUsername() {
+        let usernameValue = $('#usernames').val();
+        if (usernameValue.length == '') {
+            $('#usercheck').show();
+            usernameError = false;
+            return false;
+        } else if ((usernameValue.length < 3) ||
+            (usernameValue.length > 10)) {
+            $('#usercheck').show();
+            $('#usercheck').html("**length of username must be between 3 and 10");
+            usernameError = false;
+            return false;
+        } else {
+            $('#usercheck').hide();
+        }
+    }
+
+    // Validate Email
+    const email =
+        document.getElementById('email');
+    email.addEventListener('blur', () => {
+        let regex =
+            /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+        let s = email.value;
+        if (regex.test(s)) {
+            email.classList.remove(
+                'is-invalid');
+            emailError = true;
+        } else {
+            email.classList.add(
+                'is-invalid');
+            emailError = false;
+        }
+    })
+
+    // Submit button
+    $('#submitbtn').click(function () {
+        validateUsername();
+        validateEmail();
+        if ((usernameError == true) &&
+            (emailError == true)) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+});
+
+$("form").on("submit", function (event) {
+    alert("Your message has been received")
+    $("#myform")[0].reset();
+
+    event.preventDefault();
+});
