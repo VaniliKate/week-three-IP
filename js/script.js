32960da9d16cb7034a588f0d410613c3882af4c8
Vanili <katevanili98@gmail.com>
2022-03-11 13:56:07 +0300

    Removed the hover effects

--- js/script.js
+++ js/script.js
@@ -40,3 +40,24 @@ $(document).ready(function () {
         $("#development-hidden").toggle();
     });
 });
+
+
+
+
+function hover(mydiv) {
+    mydiv.getElementsByTagName("img")[0].style.opacity = "0.5";
+}
+
+function normal(mydiv) {
+    mydiv.getElementsByTagName("img")[0].style.opacity = "1";
+}
+
+let div = document.getElementById('div#headings1');
+
+div.innerHTML = div.innerHTML + 'Extra stuff';
+
+function myClick() {
+    text = "This text is added.";
+    document.getElementById('heading1').innerHTML =
+        document.getElementById('heading1').innerHTML + text;
+}
