a79f5ca008e4b0eb71b333e57b6afe52768a5725
Vanili <katevanili98@gmail.com>
2022-03-11 09:57:44 +0300

    Added a toggle effect to the what we do section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-    $("img").click(function(){
-        $("p").show
-    })
-})
+$(document).ready(function () {
+    $("img").click(function () {
+        $(".hidden").toggle();
+        $(".show").toggle();
+    });
+});
+$(document).ready(function () {
+    $("p").click(function () {
+        $(".show").toggle();
+        $(".hidden").toggle();
+    });
+});
