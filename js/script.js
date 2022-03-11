687d0afba9a8623ad66a50dc60ffe6f84e0a6f9f
Vanili <katevanili98@gmail.com>
2022-03-11 10:52:47 +0300

    Modified the toggles

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
+//product management toggle
+
 $(document).ready(function () {
     $("img").click(function () {
-        $(".hidden").toggle();
-        $(".show").toggle();
+        $("#product-hidden").toggle();
+        $("#product-show").toggle();
     });
 });
 $(document).ready(function () {
     $("p").click(function () {
-        $(".show").toggle();
-        $(".hidden").toggle();
+        $("#product-show").toggle();
+        $("#product-hidden").toggle();
+    });
+});
+
+//design toggle
+$(document).ready(function () {
+    $("img").click(function () {
+        $("#design-hidden").toggle();
+        $("#design-show").toggle();
+    });
+});
+$(document).ready(function () {
+    $("p").click(function () {
+        $("#design-show").toggle();
+        $("#design-hidden").toggle();
+    });
+});
+
+//development toggle
+$(document).ready(function () {
+    $("img").click(function () {
+        $("#development-hidden").toggle();
+        $("#development-show").toggle();
+    });
+});
+$(document).ready(function () {
+    $("p").click(function () {
+        $("#development-show").toggle();
+        $("#development-hidden").toggle();
     });
 });
